f21d1991d9ad317823e2480a62e2941ca3d90681
Ryan Bravo <rrbravo2@gmail.com>
2019-05-23 18:06:15 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,7 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthdy_kids.each do |kids_name, age|# add your code snippet here!
+  birthday_kids.each do |kids_name, age|# add your code snippet here!
  puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
  end
 end
